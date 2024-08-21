b85f9f19f15a52d1f2de3da740f6744dcd3dd1ca
Григорий Ким <kim-g@yandex.ru>
2024-08-21 14:13:47 +0500

    Добавьте файлы проекта.

---
+++ Orca6OutPatch/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args.Length == 0)
+{
+    Console.WriteLine("Укажите файл или директорию для конвертации");
+    Console.ReadKey();
+    return;
+}
+
+foreach (string arg in args)
+{
+    Console.WriteLine(arg);
+}
+
+Console.ReadKey();
